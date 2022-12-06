4fb0398fe8d1106b7ecd0cc9b26fc909a491c593
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-12-06 22:46:19 +0100

    Création des services de l'objet Fonction

--- Staff_Management/Staff_Management/Services/FonctionServices.cs
+++ Staff_Management/Staff_Management/Services/FonctionServices.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using Staff_Management.DAO;
+using Staff_Management.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Staff_Management.Services
 {
-    internal class FonctionServices
+    public class FonctionServices
     {
+        FonctionDAO fonctionDAO;
+
+        public FonctionServices()
+        {
+            fonctionDAO = new FonctionDAO();
+        }
+
+        public bool Exists(int id)
+        {
+            return fonctionDAO.Exists(id);
+        }
+
+        public bool Exists(string name)
+        {
+            List<Fonction> fonctions = FindAll();
+            List<Fonction> foundFonctions = fonctions.FindAll
+                (
+                    fonction => fonction.Nom == name
+                );
+
+            return foundFonctions.Count > 1;
+        }
+        public List<Fonction> FindAll()
+        {
+            return fonctionDAO.FindAll();
+        }
+
+        public Fonction FindByName(string name)
+        {
+            List<Fonction> fonctions = FindAll();
+            return fonctions.Find(fonction => fonction.Nom == name);
+        }
+
+        public Fonction Save(Fonction fonction)
+        {
+            try
+            {
+                if (!Exists(fonction.Nom))
+                    return fonctionDAO.Save(fonction);
+                else
+                {
+                    MessageBox.Show
+                        (
+                            $"La fonction '{fonction.Nom} existe déjà !",
+                            "Echec",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
+                    return null;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
     }
 }
