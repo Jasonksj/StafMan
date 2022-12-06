ff3c41601150768b14341d82cac4f98a95523534
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-12-06 21:30:28 +0100

    Création de la DAO de l'objet Fonction

--- Staff_Management/Staff_Management/DAO/FonctionDAO.cs
+++ Staff_Management/Staff_Management/DAO/FonctionDAO.cs
@@ -1,12 +1,94 @@
-﻿using System;
+﻿using Staff_Management.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Staff_Management.DAO
 {
-    internal class FonctionDAO
+    public class FonctionDAO
     {
+        StafManEntities stafMan;
+
+        Fonction fonction;
+
+        public FonctionDAO()
+        {
+            stafMan = new StafManEntities();
+            fonction = new Fonction();
+        }
+
+        public Fonction Save(Fonction fonction)
+        {
+            try
+            {
+                this.fonction = fonction;
+                stafMan.Fonctions.Add(this.fonction);
+                stafMan.SaveChanges();
+                return this.fonction;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show
+                    (
+                        $"Enregistrement impossible de la fonction '{this.fonction.Nom}'!\nErreur : {ex.Message}",
+                        "Echec",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                return null;
+            }
+        }
+
+        public int Delete(int id)
+        {
+            try
+            {
+                fonction = stafMan.Fonctions.FirstOrDefault
+                    (
+                        fonction => fonction.IdFonction == id
+                    );
+                stafMan.Fonctions.Remove(fonction);
+                stafMan.SaveChanges();
+                return fonction.IdFonction;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show
+                    (
+                        $"Suppression impossible de la fonction '{this.fonction.Nom}'!\nErreur : {ex.Message}",
+                        "Echec",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                return -1;
+            }
+        }
+
+        public bool Exists(int id)
+        {
+            try
+            {
+                return stafMan.Fonctions.SingleOrDefault(fonction => fonction.IdFonction == id) != null;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Erreur : {ex.Message}");
+            }
+        }
+
+        public List<Fonction> FindAll()
+        {
+            try
+            {
+                return stafMan.Fonctions.ToList();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Erreur : {ex.Message}");
+            }
+        }
     }
 }
