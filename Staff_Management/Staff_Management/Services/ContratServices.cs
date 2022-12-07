e1a7ec44c0e78b4faf5f0c76004d89c1774a1354
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-12-07 08:36:42 +0100

    Création des services de l'objet contrat

---
+++ Staff_Management/Staff_Management/Services/ContratServices.cs
@@ -0,0 +1,20 @@
+﻿using Staff_Management.DAO;
+using Staff_Management.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staff_Management.Services
+{
+    public class ContratServices
+    {
+        ContratDAO contratDAO;
+
+        public ContratServices()
+        {
+            contratDAO = new ContratDAO();
+        }
+    }
+}
