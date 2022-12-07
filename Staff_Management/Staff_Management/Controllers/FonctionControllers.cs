dbabe2cab8e322b56f410d8690c1ba279c0bbff9
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-12-07 17:11:48 +0100

    Création des controllers des objets des  objets Fonction et Utilisateur

---
+++ Staff_Management/Staff_Management/Controllers/FonctionControllers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Staff_Management.Services;
+using Staff_Management.Entities;
+
+namespace Staff_Management.Controllers
+{
+    public class FonctionControllers
+    {
+        FonctionServices fonctionServices;
+
+        public FonctionControllers()
+        {
+            fonctionServices = new FonctionServices();
+        }
+
+        public Fonction Save(string nom)
+        {
+            Fonction fonction = new Fonction();
+            fonction.Nom = nom;
+            return fonctionServices.Save(fonction);
+        }
+    }
+}
---
+++ Staff_Management/Staff_Management/Controllers/UtilisateurControllers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Staff_Management.Services;
+using Staff_Management.Entities;
+using System.Runtime.InteropServices;
+
+namespace Staff_Management.Controllers
+{
+    public class UtilisateurControllers
+    {
+
+        UtilisateurServices utilisateurServices;
+
+        public UtilisateurControllers()
+        {
+            utilisateurServices = new UtilisateurServices();
+        }
+
+        public Utilisateur Save(int id, string name, string password)
+        {
+            Utilisateur utilisateur = new Utilisateur();
+            utilisateur.IdUtilisateur = ++id;
+            utilisateur.NomUtilisateur = name;
+            utilisateur.MotDePasse = password;
+            return utilisateurServices.Save(utilisateur);
+        }
+    }
+}
