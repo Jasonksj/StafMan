74236dfb15c4f1168de50e862b8aaa1ed44508c2
patric237 <djoudapatric61@gmail.com>
2022-12-07 01:04:39 +0100

    add views

---
+++ Staff_Management/Staff_Management/Views/FrmSignup.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Staff_Management.Views
+{
+    partial class FrmSignup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.Confir_pass = new System.Windows.Forms.Label();
+            this.label_full = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.combo_gender = new System.Windows.Forms.ComboBox();
+            this.label_gender = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.Check_statut = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.register_but = new System.Windows.Forms.Button();
+            this.clear_but = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label1.Location = new System.Drawing.Point(233, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIGN UP";
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Location = new System.Drawing.Point(9, 67);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(55, 13);
+            this.lbl_user.TabIndex = 1;
+            this.lbl_user.Text = "Username";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Location = new System.Drawing.Point(9, 166);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(53, 13);
+            this.pass.TabIndex = 2;
+            this.pass.Text = "Password";
+            // 
+            // Confir_pass
+            // 
+            this.Confir_pass.AutoSize = true;
+            this.Confir_pass.Location = new System.Drawing.Point(9, 213);
+            this.Confir_pass.Name = "Confir_pass";
+            this.Confir_pass.Size = new System.Drawing.Size(91, 13);
+            this.Confir_pass.TabIndex = 3;
+            this.Confir_pass.Text = "Confirm Password";
+            // 
+            // label_full
+            // 
+            this.label_full.AutoSize = true;
+            this.label_full.Location = new System.Drawing.Point(9, 117);
+            this.label_full.Name = "label_full";
+            this.label_full.Size = new System.Drawing.Size(49, 13);
+            this.label_full.TabIndex = 4;
+            this.label_full.Text = "Fullname";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 229);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(158, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 182);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(158, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(97, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "User";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(4, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(54, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Admin";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(215, 132);
+            this.maskedTextBox1.Mask = "237 6 00 00 00 00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(177, 20);
+            this.maskedTextBox1.TabIndex = 12;
+            // 
+            // combo_gender
+            // 
+            this.combo_gender.FormattingEnabled = true;
+            this.combo_gender.Location = new System.Drawing.Point(215, 79);
+            this.combo_gender.Name = "combo_gender";
+            this.combo_gender.Size = new System.Drawing.Size(177, 21);
+            this.combo_gender.TabIndex = 13;
+            // 
+            // label_gender
+            // 
+            this.label_gender.AutoSize = true;
+            this.label_gender.Location = new System.Drawing.Point(212, 66);
+            this.label_gender.Name = "label_gender";
+            this.label_gender.Size = new System.Drawing.Size(42, 13);
+            this.label_gender.TabIndex = 14;
+            this.label_gender.Text = "Gender";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(416, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 202);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Panel
+            // 
+            this.Panel.Controls.Add(this.radioButton1);
+            this.Panel.Controls.Add(this.radioButton2);
+            this.Panel.Location = new System.Drawing.Point(215, 229);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(177, 31);
+            this.Panel.TabIndex = 16;
+            // 
+            // Check_statut
+            // 
+            this.Check_statut.AutoSize = true;
+            this.Check_statut.Location = new System.Drawing.Point(103, 265);
+            this.Check_statut.Name = "Check_statut";
+            this.Check_statut.Size = new System.Drawing.Size(102, 17);
+            this.Check_statut.TabIndex = 17;
+            this.Check_statut.Text = "Show Password";
+            this.Check_statut.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(215, 179);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(177, 20);
+            this.dateTimePicker1.TabIndex = 18;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(212, 163);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Birthday";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(212, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "label8";
+            // 
+            // register_but
+            // 
+            this.register_but.BackColor = System.Drawing.Color.SlateBlue;
+            this.register_but.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.register_but.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register_but.Location = new System.Drawing.Point(72, 325);
+            this.register_but.Name = "register_but";
+            this.register_but.Size = new System.Drawing.Size(159, 32);
+            this.register_but.TabIndex = 21;
+            this.register_but.Text = "REGISTER";
+            this.register_but.UseVisualStyleBackColor = false;
+            this.register_but.Click += new System.EventHandler(this.register_but_Click);
+            // 
+            // clear_but
+            // 
+            this.clear_but.BackColor = System.Drawing.Color.SlateBlue;
+            this.clear_but.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clear_but.Font = new System.Drawing.Font("Showcard Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_but.Location = new System.Drawing.Point(357, 325);
+            this.clear_but.Name = "clear_but";
+            this.clear_but.Size = new System.Drawing.Size(149, 32);
+            this.clear_but.TabIndex = 22;
+            this.clear_but.Text = "CLEAR";
+            this.clear_but.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(261, 386);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(73, 13);
+            this.linkLabel1.TabIndex = 23;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Back to Login";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Already have an account";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(413, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Choose picture";
+            // 
+            // FrmSignup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 406);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.clear_but);
+            this.Controls.Add(this.register_but);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Check_statut);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label_gender);
+            this.Controls.Add(this.combo_gender);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label_full);
+            this.Controls.Add(this.Confir_pass);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.lbl_user);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmSignup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmSignup";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Panel.ResumeLayout(false);
+            this.Panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.Label Confir_pass;
+        private System.Windows.Forms.Label label_full;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ComboBox combo_gender;
+        private System.Windows.Forms.Label label_gender;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.CheckBox Check_statut;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button register_but;
+        private System.Windows.Forms.Button clear_but;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Staff_Management/Staff_Management/Views/FrmSignup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Staff_Management.Views
+{
+    public partial class FrmSignup : Form
+    {
+        public FrmSignup()
+        {
+            InitializeComponent();
+            string[] genders = { "Male", "Female", "Mix", "No gender", "Other" };
+            combo_gender.DataSource = genders;
+            combo_gender.SelectedIndex = -1;
+            Panel.Checked = true;
+            D_T_birthday.Value = new DateTime(2000, 8, 16);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void register_but_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            new Frmlogin().Show();
+            this.Hide();
+        }
+    }
+}
