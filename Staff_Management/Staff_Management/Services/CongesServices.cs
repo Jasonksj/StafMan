bc3493cff845a8142a1f4ddad49bcc18b3becc35
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-12-08 13:48:12 +0100

    Création de la couche service de l'objet Conge

---
+++ Staff_Management/Staff_Management/Services/CongesServices.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Staff_Management.DAO;
+using Staff_Management.Entities;
+
+namespace Staff_Management.Services
+{
+    public class CongesServices
+    {
+        CongesDAO congesDAO;
+
+        public CongesServices()
+        {
+            congesDAO = new CongesDAO();
+        }
+
+        public List<Conge> FindAll()
+        {
+            return congesDAO.FindAll();
+        }
+
+        public List<Conge> FilterAllByStartDate(DateTime startDate)
+        {
+            try
+            {
+                return FindAll().FindAll(conge => conge.DateDebut == startDate);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+
+        public List<Conge> FilterAllByEndDate(DateTime endDate)
+        {
+            try
+            {
+                return FindAll().FindAll(conge => conge.DateFin == endDate);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+
+        public List<Conge> FilterByPercentLost(float percentLost)
+        {
+            try
+            {
+                return FindAll().FindAll(conge => conge.PourcentageRetrait == percentLost);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+
+        public bool Exists(int id)
+        {
+            return congesDAO.Exists(id);
+        }
+
+        public bool Exists(string justification)
+        {
+            try
+            {
+                return FindAll().Find(conge => conge.Justification == justification) != null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+
+        public Conge Save(Conge conge)
+        {
+            try
+            {
+                if (!Exists(conge.Justification))
+                    return congesDAO.Save(conge);
+                else
+                {
+                    MessageBox.Show
+                        (
+                            $"Un congé présente déjà cette justification !",
+                            "Echec",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                        );
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+
+        public Conge Update(Conge conge)
+        {
+            try
+            {
+                if (Exists(conge.IdConges))
+                    return congesDAO.Update(conge);
+                else
+                {
+                    MessageBox.Show
+                        (
+                            $"Le congé modifié n'existe pas !",
+                            "Echec",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                        );
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+
+        public int Delete(int id)
+        {
+            try
+            {
+                if (Exists(id))
+                    return congesDAO.Delete(id);
+                else
+                {
+                    MessageBox.Show
+                        (
+                            $"Ce congé n'existe pas !",
+                            "Echec",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                        );
+                    return -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+    }
+}
